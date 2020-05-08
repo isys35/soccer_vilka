e571c9bf994a0f1f8317df60b7fcb2c1771cf4cd
isys35 <isys35@mail.ru>
2020-05-08 18:34:22 +0300

    обновление тоталов добавление класса 'Вилка'

--- parsers.py
+++ parsers.py
@@ -116,7 +116,54 @@ class Pari(Bookmaker):
                 events_objs.append(event_info)
         return events_objs
 
-
+    def get_totals(self, index):
+        url = self.main_page + 'live_ar.html?hl=' + str(index) + '&hl=' + str(index) + ',&curs=0&curName=$'
+        totals = {}
+        response = self.get_response(url)
+        save_page(response.text, str(index) + 'pari_event.html')
+        soup = BeautifulSoup(response.text, 'lxml')
+        table = soup.select_one('.twp')
+        thead = table.select_one('tr')
+        table_headers = thead.select('th')
+        row1 = soup.select('.row1')
+        td_row1 = row1[0].select('td')
+        id_total = None
+        id_individ = None
+        if len(table_headers) == len(td_row1):
+            totals['main_time'] = {}
+            for table_header_id in range(len(table_headers)):
+                if 'title' in table_headers[table_header_id].attrs:
+                    if table_headers[table_header_id]['title'] == 'Тотал матча':
+                        id_total = table_header_id
+                        totals['main_time']['total'] = {float(td_row1[table_header_id].text):
+                                                            {'more': float(td_row1[table_header_id + 1].text),
+                                                             'smaller': float(td_row1[table_header_id + 2].text)}}
+                    if table_headers[table_header_id]['title'] == 'Индивидуальный тотал':
+                        id_individ = table_header_id
+                        points = td_row1[table_header_id].select('b')
+                        more_koef = td_row1[table_header_id + 1].select('u')
+                        smaller_koef = td_row1[table_header_id + 2].select('u')
+                        totals['main_time']['individ_total_1'] = {float(points[0].text):
+                                                                      {'more': float(more_koef[0].text),
+                                                                       'smaller': float(smaller_koef[0].text)}}
+                        totals['main_time']['individ_total_2'] = {float(points[1].text):
+                                                                      {'more': float(more_koef[1].text),
+                                                                       'smaller': float(smaller_koef[1].text)}}
+        bk_row1_prop = row1[1].select('.bk')
+        time_2_info = None
+        for time_info in bk_row1_prop:
+            tds_text = [td.text for td in time_info.select('td')]
+            if '2-й тайм:' in tds_text:
+                time_2_info = time_info
+                break
+        if time_2_info:
+            totals['2_time'] = {}
+            td_2_time = time_2_info.select('td')
+            if id_total:
+                totals['2_time']['total'] = {float(td_2_time[id_total].text):
+                                                            {'more': float(td_2_time[id_total + 1].text),
+                                                             'smaller': float(td_2_time[id_total + 2].text)}}
+        return totals
 
 
 class Xbet(Bookmaker):
@@ -177,6 +224,56 @@ class Xbet(Bookmaker):
             events_objs.append(event_info)
         return events_objs
 
+    @staticmethod
+    def get_id_2_time(data):
+        if 'SG' in data['Value']:  # проверяем, есть ли выбор тайма
+            selecter_time = data['Value']['SG']
+            for time in selecter_time:
+                if 'PN' in time:
+                    if time['PN'] == '2-й Тайм':
+                        time_2_id = time['I']
+                        return time_2_id
+
+    @staticmethod
+    def get_totals_from_json(data):
+        totals = {}
+        for kf in data['Value']['GE']:
+            if kf['G'] == 4:
+                totals['total'] = {}
+                for total_jsn in kf['E'][0]:
+                    totals['total'][total_jsn['P']] = {'more': total_jsn['C']}
+                for total_jsn in kf['E'][1]:
+                    totals['total'][total_jsn['P']]['smaller'] = total_jsn['C']
+            if kf['G'] == 5:
+                totals['individ_total_1'] = {}
+                for total_jsn in kf['E'][0]:
+                    totals['individ_total_1'][total_jsn['P']] = {'more': total_jsn['C']}
+                for total_jsn in kf['E'][1]:
+                    totals['individ_total_1'][total_jsn['P']]['smaller'] = total_jsn['C']
+            if kf['G'] == 6:
+                totals['individ_total_2'] = {}
+                for total_jsn in kf['E'][0]:
+                    totals['individ_total_2'][total_jsn['P']] = {'more': total_jsn['C']}
+                for total_jsn in kf['E'][1]:
+                    totals['individ_total_2'][total_jsn['P']]['smaller'] = total_jsn['C']
+        return totals
+
+    def get_totals(self, index):
+        url = 'https://1xstavka.ru/LiveFeed/GetGameZip?id={}&lng=ru&cfview=0&isSubGames=true&GroupEvents=true' \
+              '&allEventsGroupSubGames=true&countevents=250&partner=51&grMode=2'
+        totals = {}
+        main_time_url = url.format(index)
+        response = self.get_response(main_time_url)
+        data_main_time = response.json()
+        totals['main_time'] = self.get_totals_from_json(data_main_time)
+        time_2_id = self.get_id_2_time(data_main_time)
+        if time_2_id:
+            url_2_time = url.format(time_2_id)
+            response = self.get_response(url_2_time)
+            data_time_2 = response.json()
+            totals['2_time'] = self.get_totals_from_json(data_time_2)
+        return totals
+
 
 class Event:
     def __init__(self, bookmaker,
@@ -199,6 +296,7 @@ class Event:
         self.scores_2 = scores_2
         self.time = time
         self.index = index
+        self.totals = {}
 
     def __eq__(self, other):
         if isinstance(other, Event):
@@ -218,11 +316,15 @@ class Event:
         print('Счёт по таймам: {} {}'.format(self.scores_1, self.scores_2))
         print('Общий счёт: {} {}'.format(self.total_score1, self.total_score2))
 
+    def update_totals(self):
+        self.totals = self.bookmaker.get_totals(self.index)
+
 
 class Match:
     def __init__(self, event1, event2):
         self.event1 = event1
         self.event2 = event2
+        self.vilki = []
 
     def __eq__(self, other):
         if isinstance(other, Match):
@@ -245,6 +347,37 @@ class Match:
                                                           self.event2.total_score1,
                                                           self.event2.total_score2))
 
+    def show_match_totals(self):
+        print('{}  {}'.format(self.event1.bookmaker, self.event1.totals))
+        print('{}  {}'.format(self.event2.bookmaker, self.event2.totals))
+
+    def update_totals(self):
+        self.event1.update_totals()
+        self.event2.update_totals()
+        self.update_vilki()
+
+    def update_vilki(self):
+        pass
+
+
+class Vilka:
+    def __init__(self, match, time, point, total_type):
+        self.koef = None
+        self.value = None
+        self.match = match
+        self.time = time
+        self.point = point
+        self.total_type = total_type
+
+    def __eq__(self, other):
+        if isinstance(other, Vilka):
+            return (self.match == other.match and
+                    self.time == other.time and
+                    self.point == other.point and
+                    self.total_type == other.total_type)
+        return NotImplemented
+
+
 
 # поразмыслить над неймингом
 class MainApp:
@@ -266,14 +399,24 @@ class MainApp:
                 if event_1.scores_1 == event_2.scores_1 and event_1.scores_2 == event_2.scores_2:
                     match = Match(event_1, event_2)
                     print(match.show_match())
-                    if match in self.matches:
-                        pass
-                    else:
+                    if match not in self.matches:
                         self.matches.append(match)
-
+        for match in self.matches:
+            match.update_totals()
+            match.show_match_totals()
 
 if __name__ == '__main__':
     app = MainApp()
     app.run()
+    # pari = Pari()
+    # pari.update_events()
+    # print(pari.events)
+    # xbet = Xbet()
     # xbet.update_events()
-    # print(pari.events, xbet.events)
+    # print(xbet.events)
+    # for event in pari.events:
+    #     event.update_totals()
+    #     print(event.totals)
+    # for event in xbet.events:
+    #     event.update_totals()
+    #     print(event.totals)
